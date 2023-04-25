a5d0dee88813f6c2889ce718d221aa06edaf9e4f
eswaribala <parameswaribala@gmail.com>
2023-04-25 16:19:31 +0530

    updated

--- banking/stores/BankingORMStore.go
+++ banking/stores/BankingORMStore.go
@@ -1,19 +1,15 @@
 package stores
 
-import "log"
-
-package storeorm
-
 import (
-"github.com/amexws/abcbank/mockmodels"
-_ "github.com/go-sql-driver/mysql"
-"github.com/jinzhu/gorm"
-_ "github.com/jinzhu/gorm/dialects/mysql"
-"log"
+	"CiscoApr2023/day1/models"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
 )
 
-func ConnectionHelper() *gorm.DB {
-	db, err := gorm.Open("mysql", "root:vignesh@tcp(127.0.0.1:3306)/abcormdb?charset=utf8&parseTime=True")
+func ConnectionHelperORM() *gorm.DB {
+	db, err := gorm.Open("mysql", "root:vignesh@tcp(127.0.0.1:3306)/ciscobankingdb?charset=utf8&parseTime=True")
 
 	if err != nil {
 		log.Panic(err)
@@ -22,22 +18,13 @@ func ConnectionHelper() *gorm.DB {
 	return db
 }
 
-func SaveTransaction(transactionId int64, amount int64, timestamp string, sender string, receiver string) {
-	mTransaction := mockmodels.MTransaction{TransactionId: transactionId,
-		Amount: amount, Time_Stamp: timestamp,
-		Sender: sender, Receiver: receiver}
-	db := ConnectionHelper()
+func SaveTransaction(customer models.Customer) {
+
+	db := ConnectionHelperORM()
 	//generate table
-	db.AutoMigrate(mTransaction);
+	db.AutoMigrate(customer)
 	tx := db.Begin()
-
-	//To insert or create the record.
-	//NOTE: we can insert multiple records too
-	db.Debug().Save(mTransaction)
-	//Also we can use save that will return primary key
-	//resultSave:=db.Debug().Save(newUser)
+	db.Save(customer)
 	tx.Commit()
-	//fmt.Println(resultSave)
-	//log.Println(resultSave)
+	
 }
-
