9c63038f3b24521c5f9e6fbe5114eeb39da92111
eswaribala <parameswaribala@gmail.com>
2023-04-26 15:59:53 +0530

    updated

---
+++ day3/ErrorHandling.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+func capitalizeName(name string) (string, int, error) {
+	handle := func(err error) (string, int, error) {
+		return "", 0, err
+	}
+	if name == "" {
+		return handle(errors.New("no name provided"))
+	}
+	return strings.ToTitle(name), len(name), nil
+}
+func main() {
+	name, size, err := capitalizeName("sammy")
+	if err != nil {
+		fmt.Println("An error occurred:", err)
+	}
+
+	fmt.Printf("Capitalized name: %s, length: %d", name, size)
+}
