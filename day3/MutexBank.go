74067e1a9d974307500a927f1f13ae332786c2b2
eswaribala <parameswaribala@gmail.com>
2023-04-26 15:55:09 +0530

    updated

--- day3/MutexBank.go
+++ day3/MutexBank.go
@@ -7,19 +7,23 @@ import (
 
 var balance = 50000
 var m sync.Mutex
+var wgBank sync.WaitGroup
 
 func withdraw(amount int) {
+	defer wgBank.Done()
 	m.Lock()
 	if balance > amount {
 		balance = balance - amount
 	}
-	fmt.Printf("Balance%d", balance)
+	fmt.Printf("Balance%d\n", balance)
 	m.Unlock()
 }
 
 func main() {
 	fmt.Println("Starting main")
+	wgBank.Add(2)
 	go withdraw(10000)
 	go withdraw(15000)
+	wgBank.Wait()
 	fmt.Println("Return main")
 }
