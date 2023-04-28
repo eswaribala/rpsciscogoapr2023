df043e433ecd123a88ebfbf82975082273e45eb0
eswaribala <parameswaribala@gmail.com>
2023-04-28 14:54:28 +0530

    updated

--- day2/src/FundTransferStore.go
+++ day2/src/FundTransferStore.go
@@ -1,8 +1,19 @@
 package src
 
-import "CiscoApr2023/day2/models"
+import (
+	"CiscoApr2023/day2/models"
+	"math/rand"
+)
 
 func GetFTInstance() *models.FundTransfer {
 
 	return new(models.FundTransfer)
+}
+func GetFTTransactionAmount(fundTransfer *models.FundTransfer) int64 {
+
+	var fundTransaferInstance *models.FundTransfer
+	fundTransaferInstance = fundTransfer
+	fundTransaferInstance.TransactionAmount = int64(rand.Int31n(10000)) + fundTransaferInstance.TransactionAmount
+	return fundTransaferInstance.TransactionAmount
+
 }
